2d2a575bc8f5028be89425826ec00125b30fd02c
nightshade <aniekemeusanga@gmail.com>
2024-01-12 17:12:05 -0800

    memory_utils.c

--- memory_utils.c
+++ memory_utils.c
@@ -5,33 +5,44 @@
  * @ptr: Pointer passed in
  * @old_size: Old size of the array
  * @new_size: New size of the array
- * Return: Never
+ * Return: New pointer to the reallocated memory block
  */
-
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-	void *new_pointer;
-	unsigned int i = 0;
+    /* Pointer to the new memory block */
+    void *new_pointer;
 
-	if (old_size == new_size)
-		return (ptr);
+    /* Index for copying elements from old to new memory block */
+    unsigned int i = 0;
 
-	if (new_size == 0 && ptr != NULL)
-	{
-		free(ptr);
-		return (NULL);
-	}
+    /* If the old size is equal to the new size, no need to reallocate */
+    if (old_size == new_size)
+        return (ptr);
 
-	new_pointer = malloc(new_size);
+    /* If new size is 0 and the pointer is not NULL, free the memory */
+    if (new_size == 0 && ptr != NULL)
+    {
+        free(ptr);
+        return (NULL);
+    }
 
-	if (new_pointer == NULL)
-		return (NULL);
+    /* Allocate memory for the new memory block */
+    new_pointer = malloc(new_size);
 
-	while (i < old_size)
-	{
-		*((char *)new_pointer + i) = *((char *)ptr + i);
-		i++;
-	}
-	free(ptr);
-	return (new_pointer);
+    /* Check if memory allocation was successful */
+    if (new_pointer == NULL)
+        return (NULL);
+
+    /* Copy elements from the old block to the new block */
+    while (i < old_size)
+    {
+        *((char *)new_pointer + i) = *((char *)ptr + i);
+        i++;
+    }
+
+    /* Free the old memory block */
+    free(ptr);
+
+    /* Return the pointer to the new memory block */
+    return (new_pointer);
 }
--- parse.c
+++ parse.c
@@ -1,6 +1,5 @@
 #include "shell.h"
 
-
 /**
  * parse_line - Uses strtok to put args in array
  *
@@ -8,43 +7,65 @@
  *
  * Return: Double char pointer
  */
-
 char **parse_line(char *line)
 {
-	int buffer_size = BUFFERSIZE;
-	int location = 0;
-	char **tokens = NULL;
-	char *token = NULL;
+    /* Initial size for token array */
+    int buffer_size = BUFFERSIZE;
+    
+    /* Index to keep track of token location */
+    int location = 0;
 
-	tokens = malloc(sizeof(char *) * buffer_size);
-	if (!tokens)
-	{
-		perror("hsh");
-		exit(EXIT_FAILURE);
-	}
-	token = strtok(line, DELIMINATOR);
+    /* Array to store tokens */
+    char **tokens = NULL;
 
-	while (token != NULL)
-	{
-		tokens[location] = token;
-		token = strtok(NULL, DELIMINATOR);
-		location++;
-	}
-		if (location >= buffer_size)
-		{
-			buffer_size += BUFFERSIZE;
-			tokens = _realloc(tokens, BUFFERSIZE,
-					  buffer_size * sizeof(char *));
+    /* Temporary storage for each token */
+    char *token = NULL;
 
-			if (!tokens)
-			{
-				{
-					perror("hsh: allocation error\n");
-					exit(EXIT_FAILURE);
-				}
-			}
-			token = strtok(NULL, DELIMINATOR);
-		}
-	tokens[location] = NULL;
-	return (tokens);
+    /* Allocate memory for the tokens array */
+    tokens = malloc(sizeof(char *) * buffer_size);
+    if (!tokens)
+    {
+        perror("hsh");
+        exit(EXIT_FAILURE);
+    }
+
+    /* Use strtok to tokenize the input line */
+    token = strtok(line, DELIMINATOR);
+
+    /* Loop through the tokens */
+    while (token != NULL)
+    {
+        /* Store the token in the array */
+        tokens[location] = token;
+
+        /* Get the next token */
+        token = strtok(NULL, DELIMINATOR);
+
+        /* Increment the location */
+        location++;
+
+        /* Check if the array needs to be resized */
+        if (location >= buffer_size)
+        {
+            buffer_size += BUFFERSIZE;
+
+            /* Reallocate memory for the tokens array */
+            tokens = _realloc(tokens, buffer_size * sizeof(char *));
+
+            /* Check if reallocation was successful */
+            if (!tokens)
+            {
+                perror("hsh: allocation error\n");
+                exit(EXIT_FAILURE);
+            }
+
+            /* Get the next token */
+            token = strtok(NULL, DELIMINATOR);
+        }
+    }
+
+    /* Set the last element to NULL to mark the end of the array */
+    tokens[location] = NULL;
+
+    return (tokens);
 }
